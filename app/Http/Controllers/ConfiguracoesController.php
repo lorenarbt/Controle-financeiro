94bfbeb3b1151bcbd1ca33ad2cbdb716074061b7
lorena.roberta <lorena.nascimento@saeb.ba.gov.br>
2022-05-17 10:44:37 -0300

    Inicio do Projeto

---
+++ app/Http/Controllers/ConfiguracoesController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ConfiguracoesController extends Controller
+{
+    public function configuracoes(){
+        echo 'Configurações';
+    }
+}
